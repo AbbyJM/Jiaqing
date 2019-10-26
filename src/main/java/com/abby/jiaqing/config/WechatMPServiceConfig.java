66ceb29b07957b5f23086a86ba17034c79700e08
AbbyJM <1358890401@qq.com>
2019-10-26 16:47:28 +0800

    upload file

---
+++ src/main/java/com/abby/jiaqing/config/WechatMPServiceConfig.java
@@ -0,0 +1,28 @@
+package com.abby.jiaqing.config;
+
+import javax.annotation.Resource;
+import me.chanjar.weixin.mp.api.WxMpService;
+import me.chanjar.weixin.mp.api.impl.WxMpServiceImpl;
+import me.chanjar.weixin.mp.config.impl.WxMpDefaultConfigImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+
+@Configuration
+@Lazy
+public class WechatMPServiceConfig {
+    @Resource
+    private WechatProperties wechatProperties;
+
+    @Bean
+    public WxMpService wxMpService(){
+        WxMpDefaultConfigImpl config=new WxMpDefaultConfigImpl();
+        config.setAppId(wechatProperties.getAppId());
+        config.setSecret(wechatProperties.getSecret());
+        config.setToken(wechatProperties.getToken());
+        config.setAesKey(wechatProperties.getAseKey());
+        WxMpService wxMpService=new WxMpServiceImpl();
+        wxMpService.setWxMpConfigStorage(config);
+        return wxMpService;
+    }
+}
--- src/main/java/com/abby/jiaqing/web/ImageController.java
+++ src/main/java/com/abby/jiaqing/web/ImageController.java
@@ -12,7 +12,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-
+import me.chanjar.weixin.common.api.WxConsts;
+import me.chanjar.weixin.common.error.WxErrorException;
+import me.chanjar.weixin.mp.api.WxMpService;
+import me.chanjar.weixin.mp.bean.material.WxMpMaterial;
+import me.chanjar.weixin.mp.bean.material.WxMpMaterialUploadResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,8 +28,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/image")
 public class ImageController {
     Logger logger= LoggerFactory.getLogger(ImageController.class);
+
     @Resource
     private QiniuCloudService qiniuCloudService;
+
+    @Resource
+    private WxMpService wxMpService;
 
     @GetMapping(value = "/download")
     public String downloadImage(HttpServletRequest request, HttpServletResponse response){
@@ -59,7 +67,16 @@ public class ImageController {
                 System.out.println(qiniuCloudService.getImageURL(fileName));
             }
 
-
+            WxMpMaterial wxMpMaterial=new WxMpMaterial();
+            wxMpMaterial.setFile(f);
+            wxMpMaterial.setName(fileName);
+            try {
+                WxMpMaterialUploadResult result=wxMpService.getMaterialService()
+                    .materialFileUpload(WxConsts.MaterialType.IMAGE,wxMpMaterial);
+                logger.info(result.toString());
+            } catch (WxErrorException e) {
+                e.printStackTrace();
+            }
         }
         response.setContentType("application/json;utf-8");
         PrintWriter writer=response.getWriter();
