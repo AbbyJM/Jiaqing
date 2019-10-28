d6a5cb6f3c8e6206190f5b8b4d76e966fa70fe4b
AbbyJM <1358890401@qq.com>
2019-10-28 13:55:56 +0800

    fix

--- src/main/java/com/abby/jiaqing/service/impl/WxMenuServiceImpl.java
+++ src/main/java/com/abby/jiaqing/service/impl/WxMenuServiceImpl.java
@@ -2,7 +2,9 @@ package com.abby.jiaqing.service.impl;
 
 import com.abby.jiaqing.service.MenuService;
 import javax.annotation.Resource;
+import me.chanjar.weixin.common.api.WxConsts;
 import me.chanjar.weixin.common.bean.menu.WxMenu;
+import me.chanjar.weixin.common.bean.menu.WxMenuButton;
 import me.chanjar.weixin.common.error.WxErrorException;
 import me.chanjar.weixin.mp.api.WxMpService;
 import me.chanjar.weixin.mp.bean.menu.WxMpMenu;
@@ -15,7 +17,27 @@ public class WxMenuServiceImpl implements MenuService {
 
     public WxMenu createMenu() {
         WxMenu menu=new WxMenu();
-        return null;
+        WxMenuButton button1=new WxMenuButton();
+        button1.setName("嘉庆项目");
+        button1.setKey("JIAQING");
+        button1.setType(WxConsts.MenuButtonType.CLICK);
+
+        WxMenuButton button11=new WxMenuButton();
+        button11.setName("测试");
+        button11.setType(WxConsts.MenuButtonType.CLICK);
+        button1.getSubButtons().add(button11);
+
+        WxMenuButton button2=new WxMenuButton();
+        button2.setName("毕业约拍照");
+        button2.setKey("TAKE_PHOTO");
+
+        WxMenuButton button3=new WxMenuButton();
+        button3.setName("联系方式");
+        button3.setKey("CONTACT");
+        menu.getButtons().add(button1);
+        menu.getButtons().add(button2);
+        menu.getButtons().add(button3);
+        return menu;
     }
 
     public WxMpMenu getMenu() throws WxErrorException {
--- src/main/java/com/abby/jiaqing/web/MenuController.java
+++ src/main/java/com/abby/jiaqing/web/MenuController.java
@@ -6,10 +6,13 @@ import java.io.IOException;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import me.chanjar.weixin.common.bean.menu.WxMenu;
 import me.chanjar.weixin.common.error.WxErrorException;
 
+import me.chanjar.weixin.mp.api.WxMpService;
 import me.chanjar.weixin.mp.bean.menu.WxMpMenu;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,10 +25,23 @@ public class MenuController {
     @Resource
     private ObjectMapper objectMapper;
 
+    @Resource
+    private WxMpService wxMpService;
+
     @GetMapping(value = "/get")
     public void getMenu(HttpServletRequest request, HttpServletResponse response) throws WxErrorException, IOException {
         WxMpMenu wxMpMenu=menuService.getMenu();
         response.setCharacterEncoding("UTF-8");
         response.getWriter().write(wxMpMenu.toJson());
+    }
+
+    @PostMapping(value = "/new")
+    public void createNewMenu(HttpServletRequest request,HttpServletResponse response){
+        WxMenu menu=menuService.createMenu();
+        try {
+            wxMpService.getMenuService().menuCreate(menu);
+        } catch (WxErrorException e) {
+            e.printStackTrace();
+        }
     }
 }
